87b5d709ba002f3dfcbc3ed2049530ae91fc4bcc
unknown <mjsens523@gmail.com>
2024-03-05 22:08:36 -0600

    Added Py

---
+++ Python/Test/test.py
@@ -0,0 +1,17 @@
+# Variables
+
+number = 123
+
+string = "Hello"
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Loops
+
+for num in array:
+    if num % 2 == 0:
+        print(num)
+
+# Conditions
+if(12 > 10):
+    print("12 is greater than 10")
